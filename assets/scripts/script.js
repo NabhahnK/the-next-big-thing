898e919541eceb289200adf9bb4caa74998007e4
Reyah <jclipsey98@outlook.com>
2022-06-08 19:51:11 -0700

    working on fetch()

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,26 @@
 var modalBtn = document.querySelector(".modal-close");
 var modalActive = document.querySelector(".modal");
+var apiUrl = 'https://cataas.com/cat'
 
 modalBtn.addEventListener("click", function() {
     modalActive.classList.remove("is-active");
 });
 
-fetch ("")
+
+function getapi(apiUrl)
+    responce = fetch(apiUrl)
+    var data = responce.json();
+    console.log(data);
+
+
+    getapi(apiUrl);
+
+// fetch('https://cataas.com/cat', {
+// })
+    // .then(function (responce) {
+        // return responce.json();
+    // })
+    // .then(function (data) {
+        // console.log(data);
+    // })
+
