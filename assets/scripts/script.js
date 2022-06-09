d2c425608207610e28d536c300f84ca0ff5746f1
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-06-08 19:17:05 -0700

    added id to submit button and made variables for text input and submit button in js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,7 @@
 var modalBtn = document.querySelector(".modal-close");
 var modalActive = document.querySelector(".modal");
+var keyWord = document.getElementById("text-input");
+var submitBtn = document.getElementById("submit-btn");
 
 modalBtn.addEventListener("click", function() {
     modalActive.classList.remove("is-active");
