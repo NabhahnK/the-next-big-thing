c3facc6732c3179091d66a42cd5887eaa5b73aab
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-06-08 19:54:35 -0700

    made typed keyword save in local storage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,10 +3,14 @@ var modalActive = document.querySelector(".modal");
 var keyWord = document.getElementById("text-input");
 var submitBtn = document.getElementById("submit-btn");
 
+function saveKeyWord() {
+    localStorage.setItem("term", keyWord.value);
+}
+
 modalBtn.addEventListener("click", function() {
     modalActive.classList.remove("is-active");
 });
 
-submitBtn.addEventListener("click", );
+submitBtn.addEventListener("click", saveKeyWord);
 
 fetch ("")
